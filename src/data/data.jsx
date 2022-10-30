a43b0a26314dbdd5f022fc9446650053c0c9c260
Pabllot07 <pabllot07@outlook.com>
2022-10-30 18:46:41 -0300

    first commit

---
+++ src/data/data.jsx
@@ -0,0 +1,17 @@
+const leadership = [
+    {name: 'Naruto', points: 1470 },
+    {name: 'One Piece', points: 1370 },
+    {name: 'Goku', points: 1202 },
+    {name: 'Demon Slayer', points: 1007 },
+    {name: 'Bleach', points: 990 },
+    {name: 'Hunter X Hunter', points: 958 }
+];
+
+const alternative = [
+    {name: 'Naruto', points: 1210 },
+    {name: 'One Piece', points: 870 },
+    {name: 'Goku', points: 602 },
+    {name: 'Demon Slayer', points: 1007 },
+    {name: 'Bleach', points: 1390 },
+    {name: 'Hunter X Hunter', points: 378 }
+]
