3681e756793ffc8d12975cab294f422e1345ded1
Pabllot07 <pabllot07@outlook.com>
2022-10-30 19:55:32 -0300

    Navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Container} from "./styles";
 
+import { HiDotsVertical } from 'react-icons/hi'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 const Modelo = () => {
 
